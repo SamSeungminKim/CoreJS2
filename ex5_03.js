dd5815ae1bdee85b82aeced3a0c437929445a73e
김승민(Seungmin Kim) 상무 두산에너빌리티 <seungmin.kim@doosan.com>
2025-05-22 09:04:49 +0900

    ex5_03 외부 함수의 변수를 참조하는 내부 함수(3)

---
+++ ex5_03.js
@@ -0,0 +1,12 @@
+// Core JavaScript Practice by Seungmin Kim
+
+var outer = function() {
+  var a = 1;
+  var inner = function() {
+    return ++a;
+  };
+  return inner;
+};
+var outer2 = outer();
+console.log(outer2()); // 2
+console.log(outer2()); // 3
